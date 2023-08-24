f7820dbd590aaf0f41da0e19a12bc79c78595aa9
Mateus Fernandes Barbosa <mateusfb04@gmail.com>
2023-08-24 16:01:25 -0300

    Adicionando Informações básicas ao código

--- SomaDeInteiros/src/meuPacote/SomarDoisInteiros.java
+++ SomaDeInteiros/src/meuPacote/SomarDoisInteiros.java
@@ -1,7 +1,14 @@
+/*Nome: Mateus Fernandes Barbosa
+ * Objetivo: Atividade 01 de TI Back End 
+ Data: 24/08/2023*/
 package meuPacote;
+
 import java.util.*;
+
 public class SomarDoisInteiros {
+
 	public static Scanner leia = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 			
 				
